5b6d6e5ff1b1f57ccb3469340c1670f97fb98e53
Nadav Bar <nabar@microsoft.com>
2014-07-14 20:13:03 +0300

    updated speech sample to use edge.js for audio playing

--- samples/speech_stream/README.md
+++ samples/speech_stream/README.md
@@ -3,7 +3,14 @@ NodeRT sample - using speech synthesis and streams
 
 A sample script for synthesizing text to speech using the Windows.Media.SpeechSynthesis API, writing the output audio stream to file using nodert_streams, and then playing it using the media player.
 
+In order to play the audio file, this sample uses <a href="https://www.npmjs.org/package/edge" target="_blank">edge.js</a> and a code snippet from this <a href="http://tomasz.janczuk.org/2014/06/playing-audio-from-nodejs-using-edgejs.html" target="_blank">great blog post</a>.
+
 **Perquisites**:
+
+Install edge.js by running:
+```
+npm install edge
+```
 
 This sample uses the nodert-streams module which could be found in the NodeRT modules directory.
 In order to use it - clone it to your machine to a node_modules directory near the script, then build the native bindings of nodert-streams by running the following command from inside the nodert-streams directory:
--- samples/speech_stream/speak.js
+++ samples/speech_stream/speak.js
@@ -1,13 +1,21 @@
 var nodert_streams = require('nodert-streams');
 var speech = require('windows.media.speechsynthesis');
 var fs = require('fs');
-var child_process = require('child_process');
 var path = require('path');
 
-var mediaPlayerPath = '"C:\\Program Files\\Windows Media Player\\wmplayer.exe"';
-var mediaPlayerKillCmd = 'taskkill /IM wmplayer.exe';
+// taken from this great post: http://tomasz.janczuk.org/2014/06/playing-audio-from-nodejs-using-edgejs.html
+var edge = require('edge');   
+var play = edge.func(function() {/*
+     async (input) => {
+         return await Task.Run<object>(async () => {
+             var player = new System.Media.SoundPlayer((string)input);
+             player.PlaySync();
+             return null;
+         });
+    }
+*/});
+
 var outputFilePath = path.join(__dirname, 'synthesized_text.wav');
-var TIMEOUT = 5000;
 
 var synth = new speech.SpeechSynthesizer();
 synth.synthesizeTextToStreamAsync('Hello Node R T!', function(err, res) {
@@ -17,11 +25,10 @@ synth.synthesizeTextToStreamAsync('Hello Node R T!', function(err, res) {
     var inputStream = new nodert_streams.InputStream(res);
     var fileStream = fs.createWriteStream(outputFilePath);
     fileStream.on('close', function () {
-        var child = child_process.exec(mediaPlayerPath + ' ' + outputFilePath);
-
-        setTimeout(function () {
-            child_process.exec(mediaPlayerKillCmd);
-        }, TIMEOUT);
+        play(outputFilePath, function (err) {
+            if (err) return console.error(err);
+            fs.unlinkSync(outputFilePath);
+       });
     });
 
     inputStream.pipe(fileStream);
