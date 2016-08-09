a78b45414a529a2703808c289b43b4daf00d5840
Nadav Bar <nabar@microsoft.com>
2016-08-09 05:52:42 -0700

    added code to generate modules from package.json

--- src/nodert-js-wrapper/nodert_wrapper.js
+++ src/nodert-js-wrapper/nodert_wrapper.js
@@ -2,8 +2,10 @@ var fs = require('fs');
 var spawn = require('child_process').spawn;
 var os = require('os');
 var path = require('path');
-var platformConfig = require('./platform_config.json')
+var platformConfigJson = require('./platform_config.json');
+var async = require('async');
 
+var PARALLEL_JOBS_LIMIT = 1;
 var NODERT_CMD_LINE_PATH = path.join(__dirname, 'bin/NodeRTCmd.exe');
 
 function isWindows() {
@@ -20,7 +22,12 @@ function getWindowsVersion() {
   return splittedVersionString[0] + "." + splittedVersionString[1];
 }
 
+var platformConfiguration;
 function getConfigForPlatform() {
+  if (platformConfiguration) {
+    return platformConfiguration;
+  }
+
   if (!isWindows()) {
     throw new Error("Unsupported OS! NodeRT only supports Windows")
   }
@@ -28,9 +35,9 @@ function getConfigForPlatform() {
   var ver = getWindowsVersion();
   var config;
 
-  for (var k in platformConfig) {
+  for (var k in platformConfigJson) {
     if (ver.startsWith(k)) {
-      config = platformConfig[k];
+      config = platformConfigJson[k];
       break;
     }
   }
@@ -61,10 +68,12 @@ function getConfigForPlatform() {
     throw new Error("Can't find Windows.winmd file, please make sure that the Windows SDK is installed");
   }
 
-  return {
+  platformConfiguration = {
     "windowsWinMDPath": windowsWinMDPath,
-    "vsVersion" : config["vsVersion"]
-  }
+    "vsVersion": config["vsVersion"]
+  };
+
+  return platformConfiguration;
 }
 
 function getDefaultDir() {
@@ -79,19 +88,84 @@ function spawnNodeRT(config, verbose) {
   }
 
   proc = spawn(NODERT_CMD_LINE_PATH, ['--winmd', config["windowsWinMDPath"], '--namespace', config["namespace"],
-    '--outdir', config["outputDir"], '--vs', config["vsVersion"]], opts);
+    '--outdir', config["outputDir"], '--vs', config["vsVersion"], '--verbose'], opts);
   
   return proc;
 }
 
-function generateModule(namespace, outputDir) {
+function generateSingleModule(namespace, outputDir, cb) {
+  if (outputDir instanceof Function) {
+    cb = outputDir;
+    outputDir = null;
+  }
   outputDir = outputDir || getDefaultDir();
+  cb = cb || Function();
   var config = getConfigForPlatform();
   config["namespace"] = namespace;
   config["outputDir"] = outputDir;
   // for now we just run the command line
-  return spawnNodeRT(config, true);
+  var process = spawnNodeRT(config, true);
+  process.on('exit', cb);
 }
 
+function generateModules(namespaces, outputDir, cb) {
+  if (outputDir instanceof Function) {
+    cb = outputDir;
+    outputDir = null;
+  }
+  cb = cb || Function();
 
-generateModule("windows.devices.geolocation");
+  async.eachLimit(namespaces, PARALLEL_JOBS_LIMIT, function (namespace, funcCb) {
+    try {
+      generateSingleModule(namespace, outputDir, function generateModuleForEach(exitCode) {
+        var err;
+        if (exitCode != 0) {
+          error = new Error("Failed to generate module for: ", namespace);
+        }
+        funcCb(err);
+      })
+    }
+    catch (e) {
+      funcCb(e);
+    }
+  },
+  function completionCb(err) {
+    cb(err);
+  });
+}
+
+function generateFromPackageJson(packageJsonPath, outputDir, cb) {
+  if (packageJsonPath instanceof Function) {
+    cb = packageJsonPath;
+    packageJsonPath = null;
+  }
+  else if (outputDir instanceof Function) {
+    cb = outputDir;
+    
+    if (path.basename(packageJsonPath.toLowerCase()) != "package.json") {
+      packageJsonPath = null;
+      outputDir = packageJsonPath;
+
+    }
+    
+    outputDir = null;
+    packageJsonPath = null;
+  }
+
+  packageJsonPath = packageJsonPath || path.join(__dirname, "package.json");
+  cb = cb || Function();
+
+  var json = require(packageJsonPath);
+
+  var modulesToGenerate
+  if (json["_nodert"]) {
+    modulesToGenerate = json["_nodert"]["modules"];
+  }
+
+  if (modulesToGenerate) {
+    return generateModules(modulesToGenerate, outputDir, cb);
+  }
+  return cb(new Error("No NodeRT modules were specified in package.json"));
+}
+
+exports.generateModules = generateModules;
