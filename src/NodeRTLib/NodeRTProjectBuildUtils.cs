cb106858f1361e89a289103bc08996fe60e25d04
Felix Rieseberg <felix@felixrieseberg.com>
2018-09-11 18:47:00 +0200

    :wrench: Clean up build utils and generator

--- src/NodeRTLib/NodeRTProjectBuildUtils.cs
+++ src/NodeRTLib/NodeRTProjectBuildUtils.cs
@@ -1,20 +1,20 @@
-﻿// Copyright (c) Microsoft Corporation
+﻿// Copyright (c) The NodeRT Contributors
 // All rights reserved. 
 //
-// Licensed under the Apache License, Version 2.0 (the ""License""); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 
+// Licensed under the Apache License, Version 2.0 (the ""License""); you may
+// not use this file except in compliance with the License. You may obtain a
+// copy of the License at http://www.apache.org/licenses/LICENSE-2.0 
 //
-// THIS CODE IS PROVIDED ON AN  *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT. 
+// THIS CODE IS PROVIDED ON AN  *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
+// OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY
+// IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+// MERCHANTABLITY OR NON-INFRINGEMENT. 
 //
-// See the Apache Version 2.0 License for specific language governing permissions and limitations under the License.
+// See the Apache Version 2.0 License for specific language governing permissions
+// and limitations under the License.
 
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.IO;
-using System.Threading.Tasks;
-using System.Runtime.InteropServices;
 
 namespace NodeRTLib
 {
@@ -82,7 +82,7 @@ namespace NodeRTLib
             {
                 process.StartInfo.RedirectStandardOutput = true;
                 process.StartInfo.RedirectStandardError = true;
-                
+
                 Action<object, DataReceivedEventArgs> actionWrite = (sender, e) =>
                 {
                     Console.WriteLine(e.Data);
