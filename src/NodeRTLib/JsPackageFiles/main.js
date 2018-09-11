4ac359a5d27ab98875448a81c84836dc173a3404
Felix Rieseberg <felix@felixrieseberg.com>
2018-09-11 18:02:36 +0200

    :wrench: Update package files

--- src/NodeRTLib/JsPackageFiles/main.js
+++ src/NodeRTLib/JsPackageFiles/main.js
@@ -1,63 +1,67 @@
-// Copyright (c) Microsoft Corporation
+// Copyright (c) Microsoft Corporation, Nadav Bar, and Felix Rieseberg
 // All rights reserved.
 //
-// Licensed under the Apache License, Version 2.0 (the ""License""); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Licensed under the Apache License, Version 2.0 (the ""License""); you may
+// not use this file except in compliance with the License. You may obtain a
+// copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 //
-// THIS CODE IS PROVIDED ON AN  *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
+// THIS CODE IS PROVIDED ON AN  *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
+// OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY
+// IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+// MERCHANTABLITY OR NON-INFRINGEMENT.
 //
-// See the Apache Version 2.0 License for specific language governing permissions and limitations under the License.
+// See the Apache Version 2.0 License for specific language governing permissions
+// and limitations under the License.
+
 var path = require('path');
 var fs = require('fs');
 
 var npmScope = '{NpmScope}';
 
-try {
-  // this little trick makes node.js Tools for VS load intellisense for the module
-  if (fs.existsSync(path.join(__dirname, '{ProjectName}.d.js)'))) {
-    module.exports = require('./{ProjectName}.d.js');
-  }
+// this little trick makes node.js Tools for VS load intellisense for the module
+if (fs.existsSync(path.join(__dirname, '{ProjectName}.d.js)'))) {
+  module.exports = require('./{ProjectName}.d.js');
+} else {
   module.exports = require('../build/Release/binding.node');
-}
-catch(e) {
-  throw e;
 }
 
 var externalReferencedNamespaces = [{ExternalReferencedNamespaces}];
 
-
 if (externalReferencedNamespaces.length > 0) {
-    var namespaceRegistry = global.__winRtNamespaces__;
+  var namespaceRegistry = global.__winRtNamespaces__;
 
-    if (!namespaceRegistry) {
-        namespaceRegistry = {};
-        Object.defineProperty(global, '__winRtNamespaces__', {
-            configurable: true,
-            writable: false,
-            enumerable: false,
-            value: namespaceRegistry
-        });
+  if (!namespaceRegistry) {
+    namespaceRegistry = {};
+    Object.defineProperty(global, '__winRtNamespaces__', {
+      configurable: true,
+      writable: false,
+      enumerable: false,
+      value: namespaceRegistry
+    });
+  }
+
+  function requireNamespace(namespace) {
+    var moduleName = namespace.toLowerCase();
+
+    if (npmScope) {
+      moduleName = '@' + npmScope + '/' + moduleName;
     }
 
-    function requireNamespace(namespace) {
-        var moduleName = namespace.toLowerCase();
-        if (npmScope) {
-            moduleName = '@' + npmScope + '/' + moduleName;
-        }
+    var m = require(moduleName);
+    delete namespaceRegistry[namespace];
+    namespaceRegistry[namespace] = m;
+    return m;
+  }
 
-        var m = require(moduleName);
-        delete namespaceRegistry[namespace];
-        namespaceRegistry[namespace] = m;
-        return m;
-    }
+  for (var i in externalReferencedNamespaces) {
+    var ns = externalReferencedNamespaces[i];
 
-    for (var i in externalReferencedNamespaces) {
-        var ns = externalReferencedNamespaces[i];
-        if (!namespaceRegistry.hasOwnProperty(ns)) {
-            Object.defineProperty(namespaceRegistry, ns, {
-                configurable: true,
-                enumerable: true,
-                get: requireNamespace.bind(null, ns)
-            });
-        }
+    if (!namespaceRegistry.hasOwnProperty(ns)) {
+      Object.defineProperty(namespaceRegistry, ns, {
+        configurable: true,
+        enumerable: true,
+        get: requireNamespace.bind(null, ns)
+      });
     }
+  }
 }
