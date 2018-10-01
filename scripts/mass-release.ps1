3f4ec12ac4d0abe3e62d03ea1309e6b14ca04a41
Felix Rieseberg <felix@felixrieseberg.com>
2018-10-01 13:18:48 -0700

    :wrench: Add a mass release script

---
+++ scripts/mass-release.ps1
@@ -0,0 +1,28 @@
+Function Get-Folder($initialDirectory) {
+  [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+
+  $foldername = New-Object System.Windows.Forms.FolderBrowserDialog
+  $foldername.Description = "Select a folder"
+  $foldername.rootfolder = "MyComputer"
+
+  if ($foldername.ShowDialog() -eq "OK") {
+    $folder += $foldername.SelectedPath
+  }
+  return $folder
+}
+
+$dir = Get-Folder
+
+if ($dir -eq $null) {
+  return
+}
+
+$dirs = dir $dir
+
+foreach ($d in $dirs) {
+  Write-Host "Publishing $d"
+
+  cd $d
+  npm publish . --access public
+  cd $dir
+}
